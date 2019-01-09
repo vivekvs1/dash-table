6f0ed2742e258ad5a53aa75ff3d1e24be1a4863d
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2019-01-09 11:41:45 -0500

    fix header alignment regression

--- src/dash-table/derived/table/fragmentStyles.ts
+++ src/dash-table/derived/table/fragmentStyles.ts
@@ -16,24 +16,26 @@ export default (
     viewport: IDerivedData,
     rowPadding: IViewportPadding,
     scrollbarWidth: number
-): { fragment?: CSSProperties, cell?: CSSProperties}[][] => {
+): { fragment?: CSSProperties, cell?: CSSProperties }[][] => {
+    const base = [{}, { fragment: { marginRight: scrollbarWidth } }];
+
     if (!virtualization || !uiCell || !uiViewport) {
         return [
-            [{}, {}],
+            base,
             [{}, {}]
+        ];
+    } else {
+        const fullHeight = uiCell.height * viewport.data.length;
+        const virtualizedHeight = (Math.floor(uiViewport.scrollTop / uiCell.height) - rowPadding.before) * uiCell.height;
+        const headersHeight = R.sum(R.map(h => h.height, uiHeaders || []));
+
+        const marginTop = virtualization && uiViewport && uiCell ? Math.max(virtualizedHeight - headersHeight, 0) : 0;
+        const height = Math.max(fullHeight - marginTop, 0);
+
+        return [
+            base,
+            [{ cell: { marginTop } }, { fragment: { height, marginTop } }]
         ];
     }
 
-    const fullHeight = uiCell.height * viewport.data.length;
-    const virtualizedHeight = (Math.floor(uiViewport.scrollTop / uiCell.height) - rowPadding.before) * uiCell.height;
-    const headersHeight = R.sum(R.map(h => h.height, uiHeaders || []));
-
-    const marginRight = scrollbarWidth;
-    const marginTop = virtualization && uiViewport && uiCell ? Math.max(virtualizedHeight - headersHeight, 0) : 0;
-    const height = Math.max(fullHeight - marginTop, 0);
-
-    return [
-        [{}, { fragment: { marginRight } }],
-        [{ cell: { marginTop } }, { fragment: { height, marginTop } }]
-    ];
 };
